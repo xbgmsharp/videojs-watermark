60ed68f89fa52fdd79f870601c3c2d90fea20644
xbgmsharp <xbgmsharp@gmail.com>
2013-07-03 13:40:48 +0900

    Initial release

---
+++ README.md
@@ -0,0 +1,46 @@
+Video.js Watermark
+=======================
+A plugin display a watermak on top of the video with [Video.js](https://github.com/videojs/video.js/).
+
+Using the Plugin
+----------------
+The plugin automatically registers itself when you include videojs.watermark.js in your page:
+
+    <script src='videojs.watermark.js'></script>
+
+You probably want to include the default stylesheet, too. It handles showing over the image over the video:
+
+    <link href="videojs.watermark.css" rel="stylesheet">
+
+Once you have your video element created, you can activate the watermark plugin.
+The plugin take 5 options file and xpos and ypos and xrepeat and opacity.
+It use the CSS show the image over the video.
+
+    video.zoomrotate({
+        file: 'watermarks.png',
+        xpos: 50,
+        ypos: 50,
+        xrepeat: 0,
+        opacity: 0.5,
+      }
+    });
+
+or
+
+    data-setup='{ "plugins": { "watermark": { "file": "watermarks.png", "xpos": "50", "xpos": "50" } } }'
+
+Center:
+    xpos: 50
+    ypos: 50
+
+Bottom right
+    xpos: 100
+    ypos: 100
+
+Top left
+    xpos: 0
+    ypos: 0
+
+Known Issues
+------------
+It won't work when using the flash fallback.
---
+++ src/videojs.watermark.js
@@ -0,0 +1,65 @@
+console.log('watermark: Start');
+
+(function() {
+  console.log('watermark: Init defaults');
+  var defaults = {
+        file: 'Owned_Stamp.png',
+        xpos: 0,
+        ypos: 0,
+        xrepeat: 0,
+        opacity: 100,
+    },
+    extend = function() {
+      var args, target, i, object, property;
+      args = Array.prototype.slice.call(arguments);
+      target = args.shift() || {};
+      for (i in args) {
+        object = args[i];
+        for (property in object) {
+          if (object.hasOwnProperty(property)) {
+            if (typeof object[property] === 'object') {
+              target[property] = extend(target[property], object[property]);
+            } else {
+              target[property] = object[property];
+            }
+          }
+        }
+      }
+      return target;
+    };
+
+  /**
+   * register the thubmnails plugin
+   */
+  videojs.plugin('watermark', function(options) {
+    console.log('watermark: Register init');
+
+    var settings, video, div, img;
+    settings = extend(defaults, options);
+
+    /* Grab the necessary DOM elements */
+    video = this.el();
+
+    // create the watermark element
+    div = document.createElement('div');
+    img = document.createElement('img');
+    div.appendChild(img);
+    img.className = 'vjs-watermark';
+    img.src = options.file;
+    img.style.bottom = "0";
+    img.style.right = "0";
+    div.style.opacity = options.opacity;
+
+    //div.style.backgroundImage = "url("+options.file+")";
+    //div.style.backgroundPosition.x = options.xpos+"%";
+    //div.style.backgroundPosition.y = options.ypos+"%";
+    //div.style.backgroundRepeat = options.xrepeat;
+    //div.style.opacity = (options.opacity/100);
+
+    // add the watermark to the player
+    video.appendChild(div);
+
+    //video.oncontextmenu = function(){alert("Hello!");}
+    console.log('watermark: Register end');
+  });
+})();
